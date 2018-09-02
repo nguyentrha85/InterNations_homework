71aeac8c6cedf91325748bde4a3a8ae7df5e81f4
Ha Nguyen <nguyentrha@yahoo.com>
2018-09-02 20:49:47 +0100

    Adding CRUD for groups

---
+++ src/Controller/GroupsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Groups;
+use App\Form\GroupsType;
+use App\Repository\GroupsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/groups")
+ */
+class GroupsController extends AbstractController
+{
+    /**
+     * @Route("/", name="groups_index", methods="GET")
+     */
+    public function index(GroupsRepository $groupsRepository): Response
+    {
+        return $this->render('groups/index.html.twig', ['groups' => $groupsRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="groups_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $group = new Groups();
+        $form = $this->createForm(GroupsType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($group);
+            $em->flush();
+
+            return $this->redirectToRoute('groups_index');
+        }
+
+        return $this->render('groups/new.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="groups_show", methods="GET")
+     */
+    public function show(Groups $group): Response
+    {
+        return $this->render('groups/show.html.twig', ['group' => $group]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="groups_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Groups $group): Response
+    {
+        $form = $this->createForm(GroupsType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('groups_edit', ['id' => $group->getId()]);
+        }
+
+        return $this->render('groups/edit.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="groups_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Groups $group): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$group->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($group);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('groups_index');
+    }
+}
