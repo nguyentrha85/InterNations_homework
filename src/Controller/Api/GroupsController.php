a077e38768d734d3468da0e23907ffc92e3d2efb
Ha Nguyen <nguyentrha@yahoo.com>
2018-09-03 01:07:21 +0100

    Adding REST API

--- src/Controller/Api/GroupsController.php
+++ src/Controller/Api/GroupsController.php
@@ -2,18 +2,63 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as FOSRest;
+use App\Entity\Groups;
+use App\Form\GroupsType;
+use App\Repository\GroupsRepository;
 
-class GroupsController extends AbstractController
+class GroupsController extends Controller
 {
     /**
-     * @Route("/api/groups", name="api_groups")
+     * API for getting all users
+     * @FOSRest\Get("/api/groups")
+     *
+     * @return array
      */
-    public function index()
+    public function index(GroupsRepository $groupsRepository)
     {
-        return $this->render('api/groups/index.html.twig', [
-            'controller_name' => 'GroupsController',
-        ]);
+        return View::create($groupsRepository->findAll(), Response::HTTP_OK , []);
+    }
+
+    /**
+     * API for creating a new group
+     * @FOSRest\Post("/api/groups")
+     *
+     * @return array
+     */
+    public function create(Request $request)
+    {
+        $group = new Groups();
+        $group->setName($request->get('name'));
+        $group->setDescription($request->get('description'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($group);
+        $em->flush();
+
+        return View::create($group, Response::HTTP_CREATED , []);
+    }
+
+    /**
+     * API for deleting a group
+     * @FOSRest\Delete("/api/groups/{id}")
+     *
+     * @return array
+     */
+    public function delete(Request $request, GroupsRepository $groupsRepository)
+    {
+        $group = $groupsRepository->find($request->get('id'));
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($group);
+        $em->flush();
+
+        return View::create($group, Response::HTTP_OK , []);
     }
 }
--- src/Controller/Api/UsersController.php
+++ src/Controller/Api/UsersController.php
@@ -12,6 +12,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations as FOSRest;
 use App\Repository\UsersRepository;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\Users;
+use App\Form\UsersType;
 
 class UsersController extends Controller
 {
@@ -25,5 +28,41 @@ class UsersController extends Controller
     public function index(UsersRepository $usersRepository)
     {
         return View::create($usersRepository->findAll(), Response::HTTP_OK , []);
+    }
+
+    /**
+     * API for creating a new user
+     * @FOSRest\Post("/api/users")
+     *
+     * @return array
+     */
+    public function create(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new Users();
+        $encoded = $encoder->encodePassword($user, $request->get('password'));
+        $user->setPassword($encoded);
+        $user->setName($request->get('name'));
+        $user->setUsername($request->get('username'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return View::create($user, Response::HTTP_CREATED , []);
+    }
+
+    /**
+     * API for deleting a new user
+     * @FOSRest\Delete("/api/users/{id}")
+     *
+     * @return array
+     */
+    public function delete(Request $request, UsersRepository $usersRepository)
+    {
+        $user = $usersRepository->find($request->get('id'));
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return View::create($user, Response::HTTP_OK , []);
     }
 }
